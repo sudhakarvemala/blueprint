d398365a10dba88028b5906b5b977a96e62cd6c3
sudhakarvemala <sudhakar.vemala2@gmail.com>
2017-09-10 11:25:32 +0530

    change the new code

--- login.java
+++ login.java
@@ -1,1 +1,25 @@
-hello my first file source code management 
+<properties>
+<foo> bar</foo>
+</properties>
+
+Project Dependencies
+
+<project>
+<dependencies>
+<groupid>org.codehaus.xfire</groupid>
+<artifactid>xfire-java5</artifactid>
+<version>1.2.5</version>
+</dependency>
+<dependency>
+<groupid>junit</groupid>
+<artifactid>junit</artifactid>
+<version>3.8.1</version>
+<scope>test</scope>
+</dependency>
+<groupid>javax.servlet</groupid>
+<artifactid>servletapi</artifactid>
+</version>2.4</version>
+<scope>providev</scope>
+</dependency>
+</dependencies>
+</project>
